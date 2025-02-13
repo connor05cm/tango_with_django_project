b35578d9f31e2c6032e62c92bc3ce713b16672ae
“Connor <2885877M@student.gla.ac.uk>
2025-02-13 13:23:52 +0000

    chapter 4, some changes to 3

--- rango/views.py
+++ rango/views.py
@@ -3,9 +3,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    <a href='/rango/about/'>About</a>
-    return HttpResponse("Rango says hey there partner!")
+    context_dict = {"boldmessage": "Crunchy, creamy, cookie, candy, cupcake"}
+    return render(request, "rango/index.html", context = context_dict)
     
 def about(request):
-    <a href='/rango/'>Index</a>
-    return HttpResponse("Rango says here is the about page")
+    return render(request,"rango/about.html")
